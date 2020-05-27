16a322d5a4b9301824bfd5e95f919efef3bc9fe4
Quỳnh Châu <lvqchau@apcs.vn>
2020-05-27 16:52:52 +0700

    [FE] Noti: init NotiScreen

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -2,8 +2,8 @@ const COLORS = {
   salmon: '#FF6B6A',
   darkblue: '#6673A0',
   lightgray: '#7D9D9C',
-  aqua: '#4A7EFB',
-  gray: '#EAEAEA'
+  aqua: '#00C4B5',
+  gray: '#ADADAD'
 }
 
-export default COLORS;
+export default COLORS
