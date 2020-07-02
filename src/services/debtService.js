67d9cff9c84516ea980ccf406637f7431dd094fb
Quỳnh Châu <lvqchau@apcs.vn>
2020-07-02 17:56:17 +0700

    Fix: debtservice

---
+++ src/services/debtService.js
@@ -0,0 +1,9 @@
+import axios from './axios'
+import { baseURL } from '../assets/constant/constant'
+import AsyncStorage from '@react-native-community/async-storage'
+
+export class debtManagingService {
+  
+}
+
+export const debt = new debtManagingService();
